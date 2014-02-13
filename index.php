00622d05f5355e8889babd91eb49987661bb0f5a
DURU <enfn2001@gmail.com>
2014-02-13 14:32:20 +0900

    error catch using try~catch statement

--- index.php
+++ index.php
@@ -1,2 +1,8 @@
 <?php
+try {
 	$handler = new PDO('mysql:host=127.0.0.1;dbname=app','root','');
+	$handler->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+	echo 'Caught';
+}
+	
