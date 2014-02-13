e21cd47a0403cae32b74892feef123599ce8d57f
DURU <enfn2001@gmail.com>
2014-02-13 14:45:46 +0900

    connection database using PDO

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 try {
-	$handler = new PDO('mysql:host=127.0.0.1;dbname=app','root','');
+	$handler = new PDO('mysql:host=127.0.0.1;dbname=app','root','111111');
 	$handler->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
 	echo $e->getMessage();
