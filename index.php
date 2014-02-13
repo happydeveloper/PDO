3f3ded51cb7e3d70cb4a885c664da3f7da35b2d1
DURU <enfn2001@gmail.com>
2014-02-13 14:35:43 +0900

    catch error then database close to present message

--- index.php
+++ index.php
@@ -3,6 +3,6 @@ try {
 	$handler = new PDO('mysql:host=127.0.0.1;dbname=app','root','');
 	$handler->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
-	echo 'Caught';
+	die('sorry database problem');
 }
 	
