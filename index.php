c5567b4d72a0f88da237627d9981a187f7005616
DURU <enfn2001@gmail.com>
2014-02-13 14:43:49 +0900

    present to error message

--- index.php
+++ index.php
@@ -3,6 +3,9 @@ try {
 	$handler = new PDO('mysql:host=127.0.0.1;dbname=app','root','');
 	$handler->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
-	die('sorry database problem');
+	echo $e->getMessage();
+	die();
 }
+
+echo 'The rest of our page.';
 	
