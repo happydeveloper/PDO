24d5a34d83adba9efbac253b13ad7854b88ebc00
DURU <enfn2001@gmail.com>
2014-02-17 14:51:12 +0900

    PDO를 이용해서 데이타베이스로 부터 데이타인출

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 try {
-//	print_r(PDO::getAvailableDrivers());	
 	$db = new PDO('mysql:host=127.0.0.1;dbname=app','root','111111');
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }catch(PDOException $e) {
@@ -8,8 +7,9 @@ try {
 	die('sorry, database problem');
 }
 
-	echo "the rest of page";
+$qry = $db->query('SELECT * FROM guestbook');
 
-?>
-
+while($r = $qry->fetch()) {
+	echo $r['message'], '<br>';
+}
 
