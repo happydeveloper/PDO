0f63b5198943c8aecaa475c1c92f986925a8524a
DURU <enfn2001@gmail.com>
2014-02-17 14:25:11 +0900

    pdo로 이용해서 데이타베이스 정상적인 연결

--- index.php
+++ index.php
@@ -1,15 +1,13 @@
 <?php
 try {
 	print_r(PDO::getAvailableDrivers());	
-	$db = new PDO('mysql:host=127.0.0.1;dbname=appp','root','111111');
+	$db = new PDO('mysql:host=127.0.0.1;dbname=app','root','111111');
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }catch(PDOException $e) {
 	echo $e->getMessage();
 	die('sorry, database problem');
 }
 
-$query = $db->query('SELECT * FROM guestbook');
+$echo 'the rest of page'
 
-$r = $query->fetch();
 
-var_dump($r);
