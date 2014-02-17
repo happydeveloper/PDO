5f9dd4e6f00a95dcff6cc6d7f130b9623b419735
DURU <enfn2001@gmail.com>
2014-02-17 14:45:44 +0900

    에러 없이 데이타베이스 연결한 예제

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 try {
-	print_r(PDO::getAvailableDrivers());	
+//	print_r(PDO::getAvailableDrivers());	
 	$db = new PDO('mysql:host=127.0.0.1;dbname=app','root','111111');
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }catch(PDOException $e) {
@@ -8,6 +8,8 @@ try {
 	die('sorry, database problem');
 }
 
-$echo 'the rest of page'
+	echo "the rest of page";
+
+?>
 
 
