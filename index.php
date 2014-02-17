420793403a26362fab41585d992458bb256033ba
DURU <enfn2001@gmail.com>
2014-02-17 10:45:34 +0900

    pdo 이용가능한 드라이버 확인 작업시

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
-	
+try {
+	print_r(PDO::getAvailableDrivers());	
 	$db = new PDO('mysql:host=127.0.0.1;dbname=app','root','111111');
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}catch(PDOException $e) {
+	echo $e->getMessage();
+	die();
+}
 
+$query = $db->query('SELECT * FROM guestbook');
+
+$r = $query->fetch();
+
+var_dump($r);
